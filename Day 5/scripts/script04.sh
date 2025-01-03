c0f13b48c1b64dc806351e72dec634166ebd40e0
raju1220 <123093185+raju1220@users.noreply.github.com>
2025-01-04 01:08:55 +0530

    Delete Day 5 directory

--- Day 5/scripts/script04.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Home directory : $HOME"
-echo "Login Name : $LOGNAME"
-echo "Path : $PATH"
-echo "Current working directory : $PWD"
-echo "Old working directory : $OLDPWD"
-echo "Current working directory : $PWD"
